91c6a0fe478c4a8337773a97a920b5b43b72ab77
Peter Simon <psimon0420@gmail.com>
2026-06-25 13:18:47 -0700

    Add docstrings

--- src/FSweep.jl
+++ src/FSweep.jl
@@ -1,6 +1,22 @@
-#module FSweep # 
+"""
+    FSweep
+
+Module implementing matrix format tangential interpolation based on the following references:
+
+[1] Shilpa, T. N., and Rakesh Sinha. "Fully Adaptive and Semi-Adaptive Frequency Sweep Algorithm
+Exploiting Loewner-State Model for EM Simulation of Multiport Systems." IEEE Transactions on
+Microwave Theory and Techniques, vol. 73, no. 9, Sept. 2025.
+
+[2] S. T. N and R. Sinha, "An Adaptive Frequency Sweep Algorithm for Broadband EM Simulation Incorporating
+Random Matrix Transformation in the Loewner Framework," 2025 IEEE Microwaves, Antennas, and Propagation
+Conference (MAPCON), Kochi, India, 2025, doi: 10.1109/MAPCON65020.2025.11426247.
+
+[3] S. T. N and R. Sinha, "An Adaptive Frequency Sweep Algorithm Employing Additive Perturbation in the
+Loewner Matrix Model for Electromagnetic Simulations," 2025 55th European Microwave Conference (EuMC),
+Utrecht, Netherlands, 2025, pp. 723-726, doi: 10.23919/EuMC65286.2025.11235258.
+"""
+#module FSweep #
 #
 #greet() = print("Hello World!")
 #
 #end # module FSweep
-
