dd4c9d001b14532c451ab853983865f56cd10698
Peter Simon <psimon0420@gmail.com>
2026-06-28 16:39:12 -0700

    Add docstrings to randomLU.jl

--- src/randomLU.jl
+++ src/randomLU.jl
@@ -1,9 +1,17 @@
+"""
+    random_nonsingular_matrix(n::Integer, T::DataType = ComplexF64)
+Create a random, nonsingular matrix of order `n` and whose `eltype` is `T`.
+"""
 function random_nonsingular_matrix(n::Integer, T::DataType = ComplexF64)
     mat = rand(T, n, n)
     mat = LowerTriangular(mat) * UpperTriangular(mat)
     return mat
 end
 
+"""
+    loewner_random_model(L, σL, V, W)
+Overwrite previouisly defined method with one that uses nonsingular random matrices.
+"""
 function loewner_random_model(L, σL, V, W)
     n = size(L, 1)
     Wa = random_nonsingular_matrix(n)
@@ -16,4 +24,8 @@ function loewner_random_model(L, σL, V, W)
     return E, A, B, C
 end
 
+"""
+    eval_random_model!(H, M, X, E, A, B, C, s)
+Overwrite previously defined method to use `eval_reduced_model!` (and hence LU decomposition).
+"""
 eval_random_model!(H, M, X, E, A, B, C, s) = eval_reduced_model!(H, M, X, E, A, B, C, s)
